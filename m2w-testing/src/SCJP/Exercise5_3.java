29a3ffb7beadf0da0062774e1d204cf27f82d749
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-06-18 03:11:30 +0000

    same turns merged .

---
+++ m2w-testing/src/SCJP/Exercise5_3.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SCJP;
+//import java.lang.*;
+
+import java.lang.Exception;
+
+
+/**
+ *
+ * @author ruobo
+ */
+public class Exercise5_3 {
+
+    public static void main(String[] args){
+        Exercise5_3 e = new Exercise5_3();
+        try{
+            e.reverse("");
+        }catch (Exception a){
+            a.printStackTrace();
+            System.out.println("s = empty");
+        }
+        finally{
+            System.out.println("finished");
+        }
+    }
+    private Exception Exception;
+
+
+    String reverse(String s) throws Exception{
+        String reverseStr = "";
+        
+        if(s.length() == 0){
+            throw Exception;
+        }
+
+
+        for(int i = s.length()-1; i >=0; --i){
+            reverseStr += s.charAt(i);
+        }
+        
+        return reverseStr;
+    }
+}
