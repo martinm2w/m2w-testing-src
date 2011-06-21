15555f2957d1df32cae8fd121a37eca829281324
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-06-21 02:38:24 +0000

    same turns merged .

---
+++ m2w-testing/src/SCJP/Exercise5_4.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SCJP;
+
+/**
+ *
+ * @author ruobo
+ * @date 6/20/11 9:12 PMl
+ */
+public class Exercise5_4 {
+
+}
+
+class BadFoodException extends Exception{
+    
+}
+
+class MyException{
+
+    public static void main(String[] args){
+        MyException me = new MyException();
+        try{
+            System.out.println("trying input : toufu");
+            me.checkFood("toufu");
+            System.out.println("trying input : smelly toufu");
+            me.checkFood("smelly toufu");
+        }catch(BadFoodException bf){
+            bf.printStackTrace();
+            System.out.println("bah");
+        }
+    }
+
+    public void checkFood(String food) throws BadFoodException{
+        if(food.equals("toufu")){
+            System.out.println("yay");
+        }else{
+            throw new BadFoodException();
+            
+        }
+    }
+}
