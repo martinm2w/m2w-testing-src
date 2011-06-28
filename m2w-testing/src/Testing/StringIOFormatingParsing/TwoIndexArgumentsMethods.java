fd22dd7db7453959b592d3b2960ce78957e4b6a8
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-06-28 03:05:55 +0000

    from home

---
+++ m2w-testing/src/Testing/StringIOFormatingParsing/TwoIndexArgumentsMethods.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Testing.StringIOFormatingParsing;
+
+/**
+ *
+ * @author ruobo
+ * @date Jun 27, 2011
+ */
+public class TwoIndexArgumentsMethods {
+
+}
