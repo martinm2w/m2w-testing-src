eaa86235efbfe6d5b64fe1318b6904c710919f9d
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-07-05 03:43:58 +0000

    from home

--- m2w-testing/src/Testing/RegexAndSearching/TestingRegex.java
+++ m2w-testing/src/Testing/RegexAndSearching/TestingRegex.java
@@ -13,10 +13,19 @@ import java.util.regex.*;
  */
 public class TestingRegex {
     public static void main(String[] args){
+        TestingRegex tr = new TestingRegex();
+        tr.scjpChp6_exc1();
+    }
+
+    private void scjpChp6_text(){
         Pattern p = Pattern.compile("\\d\\w");
         Matcher m = p.matcher("ab4 56_7ab");
         System.out.println("Pattern is: " + m.pattern());
         while(m.find()){
+//       Find:     Attempts to find the next subsequence of the input sequence that matches the pattern.
+//            This method starts at the beginning of this matcher's region,
+//            or, if a previous invocation of the method was successful and the matcher has not since been reset, at the first character not matched by the previous match.
+//            If the match succeeds then more information can be obtained via the start, end, and group methods.
             System.out.println("start: " + m.start() + ", ends: " + m.end() + ", " + m.group());
             //For a matcher m with input sequence s,
             //the expressions m.group() and s.substring(m.start(), m.end()) are equivalent.
@@ -31,5 +40,20 @@ public class TestingRegex {
             //For a matcher m with input sequence s,
             //the expressions m.group() and s.substring(m.start(), m.end()) are equivalent.
         }
+    }
+
+    private void scjpChp6_exc1(){
+        Pattern p = Pattern.compile("\\d*");
+        Matcher m = p.matcher("ab34ef");
+        boolean b = false;
+
+        while(b = m.find()){
+            System.out.print(m.start() + m.group());
+            //testing:
+            System.out.println();
+//            System.out.println(" count:  " +  m.groupCount());
+        }
+        
+
     }
 }
