6d851b32206135e2b7855dfae50c92c6be9878c8
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-07-14 00:50:49 +0000

    from home

---
+++ trunk/m2w-testing/src/SCJP/Chp7Q1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SCJP;
+
+import java.util.*;
+/**
+ *
+ * @author ruobo
+ * @date Jul 11, 2011
+ */
+public class Chp7Q1 {
+    public static void main(String[] args){
+        Chp7Q1 exc = new Chp7Q1();
+        exc.chp7q1();
+    }
+    
+    private void chp7q1(){
+//        List<List<Integer>> table = new List<List<Integer>>();//list is abstract cannot be instantiated
+        List<List<Integer>> table = new ArrayList<List<Integer>>();//correct
+//        List<List<Integer>> table = new ArrayList<ArrayList<Integer>>();//type arguement <List<Integer>> must be the same for both sides of the assignment.
+//        List<List, Integer> table = ...// can only take 1 arg
+        
+    }
+}
