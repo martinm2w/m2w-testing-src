14e28759adff5e4971dc1983c8c9d5be22824307
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-07-20 03:31:19 +0000

    from home

---
+++ trunk/m2w-testing/src/Testing/util/SOP.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Testing.util;
+
+/**
+ *
+ * @author ruobo
+ * @date Jul 19, 2011
+ */
+public class SOP {
+    public static <T> void sop(T t){
+        System.out.println(t.toString());
+    }
+    public static void sopo(Object o ){
+        System.out.println(o.toString());
+    }
+}
