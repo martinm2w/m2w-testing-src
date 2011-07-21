c5df97489cb7ee08f36084c672be36aca691952d
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-07-21 02:42:49 +0000

    from home

---
+++ trunk/m2w-testing/src/SCJP/Exercise9_1.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SCJP;
+
+/**
+ *
+ * @author ruobo
+ */
+public class Exercise9_1 extends Thread{
+
+    public static void main(String[] args){
+        Exercise9_1 m2w = new Exercise9_1();
+        m2w.start();
+    }
+
+    @Override
+    public void run(){
+        for(int i = 0; i++ < 100; ){
+            if((i % 10) == 0){
+                System.out.println(i);
+            }
+            try{
+                Thread.sleep(1000);
+            }catch(Exception e){e.printStackTrace();}
+        }
+    }
+}
+
+class EX9_1_1 implements Runnable{
+    @Override
+    public void run(){
+        for(int i = 0; i++ < 100;){
+           if((i % 10) == 0){
+                System.out.println(i);
+            }
+            try{
+                Thread.sleep(100);
+            }catch(Exception e){e.printStackTrace();}
+        }
+    }
+    public static void main(String[] args){
+        EX9_1_1 run = new EX9_1_1();
+        Thread m2w1 = new Thread(run);
+        m2w1.start();
+        for(Object O : m2w1.getStackTrace())
+            System.out.println(O);
+    }
+}
