c357ac3ee0778e0ce995417cf88a01f7b04f29fa
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-07-23 16:12:34 +0000

    from home, 2nd mock 1st commit

---
+++ trunk/m2w-testing/src/Testing/_02_OO_Inher_Poly/Interface_Abstract/TestingAbstractClassDeclare.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Testing._02_OO_Inher_Poly.Interface_Abstract;
+
+/**
+ * 1. abstact keyword must come before the "class"
+ * @author ruobo
+ * @date Jul 22, 2011
+ */
+public class TestingAbstractClassDeclare {
+    
+}
+
+//class abstract  A_abs{}//fail
+ abstract class A_abs{}
