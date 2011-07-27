fd3057b1ee9225c493aa71d92d7d71e2b3d702c6
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-07-27 03:28:25 +0000

    reconstructed the enum into chap1

--- m2w-testing/src/Testing/_02_OO_Inher_Poly/Inheritance/TestingOverridingToStringMethod.java
+++ m2w-testing/src/Testing/_02_OO_Inher_Poly/Inheritance/TestingOverridingToStringMethod.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Testing._00_Other;
+package Testing._02_OO_Inher_Poly.Inheritance;
 
 /**
  *
