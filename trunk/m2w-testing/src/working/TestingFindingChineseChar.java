cb1661ed181975408c318356b0c827b4535c9d2b
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-08-04 15:16:10 +0000

    lab

---
+++ trunk/m2w-testing/src/working/TestingFindingChineseChar.java
@@ -0,0 +1,23 @@
+package working;
+
+/**
+ *
+ * @author [Ruobo + m2w]
+ * @date [Aug 3, 2011 - 1:33:11 PM]
+ */
+public class TestingFindingChineseChar {
+    public static void main(String[] args){
+        String s = "个人觉得其实最重要的是要乐观";
+//        boolean b = s.matches("[\\u4E00-\\u9FA5]");
+        StringBuilder ss = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            Character a = s.charAt(i);
+            String temp = a.toString();
+            if (temp.matches("[\\u4E00-\\u9FA5]"))
+                ss.append(temp);
+        }
+        System.out.println(ss);
+//        if (b) System.out.println("found!");
+//        else System.out.println("fail");
+    }
+}
