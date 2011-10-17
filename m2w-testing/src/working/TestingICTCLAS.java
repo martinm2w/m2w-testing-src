375fb9ce851795809d7f7cf01691066e3adb67ce
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2011-10-17 15:26:21 +0000

    10-17 updated cvs checkout, modified for chinese communicationlinkX using 2008 version pos tagger and segementer

--- m2w-testing/src/working/TestingICTCLAS.java
+++ m2w-testing/src/working/TestingICTCLAS.java
@@ -1,27 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package working;
-
-import org.ictclas4j.bean.SegResult;
-import org.ictclas4j.segment.SegTag;
-
-/**
- *
- * @author ruobo
- */
-public class TestingICTCLAS {
-    public static void main(String[] args){
-        System.out.println("This is OneMain");
-
-        SegTag st = new SegTag(1);
-
-                SegResult sr = st.split("asdf cvasdftqw34 a asdfkl;jasd;fl kj");
-//        SegResult sr = st.split("一块勤奋地漂亮的一块钱,/打造经济的航空母舰。ABCD.#$% Hello World!\n又一段文本123辆 ！3.0");
-
-        System.out.println(sr.getFinalResult());
-
-        }
-
-}
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package working;
+//
+//import org.ictclas4j.bean.SegResult;
+//import org.ictclas4j.segment.SegTag;
+//
+///**
+// *
+// * @author ruobo
+// */
+//public class TestingICTCLAS {
+//    public static void main(String[] args){
+//        System.out.println("This is OneMain");
+//
+//        SegTag st = new SegTag(1);
+//
+//                SegResult sr = st.split("asdf cvasdftqw34 a asdfkl;jasd;fl kj");
+////        SegResult sr = st.split("一块勤奋地漂亮的一块钱,/打造经济的航空母舰。ABCD.#$% Hello World!\n又一段文本123辆 ！3.0");
+//
+//        System.out.println(sr.getFinalResult());
+//
+//        }
+//
+//}
