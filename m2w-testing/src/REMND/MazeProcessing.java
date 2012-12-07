c49a1372adea1b9764965e8e71860cd9a082898e
martinm2w@gmail.com <martinm2w@gmail.com@38a2238a-4551-c310-5230-1903f1d97c3e>
2012-12-07 16:03:58 +0000

    champs final work

---
+++ m2w-testing/src/REMND/MazeProcessing.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package REMND;
+
+/**
+ *
+ * @author ruobo
+ * @lastupdate Dec 7, 2012
+ */
+public class MazeProcessing {
+//==============================constructor=====================================
+//=============================public methods===================================
+//============================private methdos===================================
+//===========================instance variables=================================
+//============================setters & getters=================================
+}
---
+++ m2w-testing/src/sdfsf.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ruobo
+ * @lastupdate Sep 23, 2012
+ */
+public class sdfsf {
+//==============================constructor=====================================
+    public static void main(String[] args){
+//        for(int i = 0; i < 10; i++){
+//            for(int j = i-1; j >= 0; j--){
+//                System.out.println(i +" "+ j);
+//            }
+//        }
+         for(int i = 0; i < 10; i++){
+            for(int j = 0; j < i; j++){
+                System.out.println(i +" "+ j);
+            }
+        }
+    }
+//=============================public methods===================================
+//============================private methdos===================================
+//===========================instance variables=================================
+//============================setters & getters=================================
+}
